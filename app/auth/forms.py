bda9b1ee6b9f2d2c25ec8c20dc879db275f7d5fb
IreriVikki <wambsviki@gmail.com>
2018-09-06 14:54:53 +0300

    added a regestration form complete with the models and the template

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,15 +1,22 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import Email, Length, Required
+from wtforms.validators import Email, Length, DataRequired
 
 
 class LoginForm(FlaskForm):
-    username = StringField('Email', validators=[Email()])
+    email = StringField('Email', validators=[Email(), DataRequired()])
     password = PasswordField('Password', validators=[
-                             Required(), Length(min=3, max=20)])
+                             DataRequired(), Length(min=3, max=20)])
     remember = BooleanField('Remember Me')
     submit = SubmitField('Submit')
 
 
 class RegisterForm(FlaskForm):
-    pass
+    email = StringField('Email', validators=[Email(), DataRequired()])
+    username = StringField('Username', validators=[
+                           Length(min=2, max=20), DataRequired()])
+    password = PasswordField('Password', validators=[
+                             DataRequired(), Length(min=3, max=20)])
+    confirm_password = PasswordField('Confirm Password', validators=[
+        DataRequired(), Length(min=3, max=20)])
+    submit = SubmitField('Register')
