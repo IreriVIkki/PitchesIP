f496dfee216bdb7c5d5ba940f3b6cc3024e5b88e
IreriVikki <wambsviki@gmail.com>
2018-09-06 22:21:25 +0300

    added a Pitch model for creating database content

--- app/models.py
+++ app/models.py
@@ -11,3 +11,21 @@ class Category(db.Model):
 
     def __repr__(self):
         return f'Category {self.category_name}'
+
+
+class Pitch(db.Model):
+    __tablename__ = 'pitches'
+
+    id = db.Column(db.String, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    content = db.Column(db.String, nullable=False)
+    rating = db.Column(db.Interger, nullable=False)
+    likes = db.Column(db.Integer, nullable=False)
+    dislikes = db.Column(db.Integer, nullable=False)
+    time = db.Column(db.DateTime, default=datetime.utcnow)
+    author_id = db.Column(db.Interger, db.ForeignKey('users.id'))
+    category_id = db.Column(db.Integer, db.ForeignKey('categories.id'))
+    comment_id = db.Column(db.Integer, db.ForeignKey('comments.id'))
+
+    def __repr__(self):
+        return f'Pitch {self.title}, {self.content}, {self.rating}, {self.likes}, {self.dislikes}, {self.time}'
