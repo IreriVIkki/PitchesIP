4a8f788b2a530f6f0aaa87d2c19a255de88cc548
IreriVikki <wambsviki@gmail.com>
2018-09-06 11:29:52 +0300

    Finalised a basic app structure that runs in the browser perfectly from manager file

--- app/__init__.py
+++ app/__init__.py
@@ -6,4 +6,10 @@ def create_app(config_name):
 
     app = Flask(__name__)
     app.config.from_object(config_options[config_name])
-    config_options[config_name].init_app(app)
+    # config_options[config_name].init_app(app)
+
+    # importing and regestering my main blueprint
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    return app
--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 class DevConfig:
+    DEBUG = True
     pass
 
 
