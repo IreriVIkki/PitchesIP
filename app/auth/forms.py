8923da4ec7f0855c0dec05b71ce27c4a2ab2cc72
IreriVikki <wambsviki@gmail.com>
2018-09-07 18:59:04 +0300

    successfully made the register button add a new user to the database

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,31 +1,21 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, BooleanField, ValidationError
-from wtforms.validators import Email, Length, DataRequired
-from ..models import User
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Length, Email, EqualTo
 
 
 class LoginForm(FlaskForm):
-    email = StringField('Email', validators=[Email(), DataRequired()])
-    password = PasswordField('Password', validators=[
-                             DataRequired(), Length(min=3, max=20)])
-    remember = BooleanField('Remember Me')
-    submit = SubmitField('LogIn')
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember = BooleanField('Remeber Me')
+    submit = SubmitField('Login')
 
 
-class RegisterForm(FlaskForm):
-    email = StringField('Email', validators=[Email(), DataRequired()])
+class RegistrationForm(FlaskForm):
+
     username = StringField('Username', validators=[
-                           Length(min=2, max=20), DataRequired()])
-    password = PasswordField('Password', validators=[
-                             DataRequired(), Length(min=3, max=20)])
-    confirm_password = PasswordField('Confirm Password', validators=[
-        DataRequired(), Length(min=3, max=20)])
+                           DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField(
+        'Password', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign Up')
-
-    def validate_email(self, data_field):
-        if User.query.filter_by(email=data_field.data).first():
-            raise ValidationError('There is an account with that email')
-
-    def validate_username(self, data_field):
-        if User.query.filter_by(name=data_field.data).first():
-            raise ValidationError('That user name is taken')
--- app/auth/views.py
+++ app/auth/views.py
@@ -1,24 +1,21 @@
-from flask import render_template, url_for, redirect
-from .forms import RegisterForm, LoginForm
+from flask import render_template, redirect, url_for
 from . import auth
-from .. import db
-from ..models import User
+from .forms import LoginForm, RegistrationForm
+from ..models import db, User
 
 
 @auth.route('/login')
 def login():
-    form = LoginForm()
-    return render_template('auth/login.html', form=form)
+    return render_template('auth/login.html')
 
 
 @auth.route('/register', methods=['GET', 'POST'])
 def register():
-    form = RegisterForm()
+    form = RegistrationForm()
     if form.validate_on_submit():
-        print('WORKS')
-        user = User(email=form.email.data, name=form.name.data,
+        user = User(email=form.email.data, name=form.username.data,
                     password=form.password.data)
         db.session.add(user)
         db.session.commit()
-        return redirect(url_for('auth.login'))
-    return render_template('auth/register.html', form=form)
+        return redirect(url_for('main.home'))
+    return render_template('auth/register.html', title='Register', form=form)
