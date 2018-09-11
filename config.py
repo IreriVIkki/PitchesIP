b7f34a6ce04c49eb32e9b92b584574ded1bbf586
IreriVikki <wambsviki@gmail.com>
2018-09-11 12:28:14 +0300

    deploy to heroku

--- config.py
+++ config.py
@@ -3,14 +3,12 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://vikki:sasawa@localhost/pitches'
+    DATABSQLALCHEMY_DATABASE_URIASE_URL = 'postgresql+psycopg2://vikki:sasawa@localhost/pitches'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     # simple mde configurations
     SIMPLEMDE_JS_IIFE = True
     SIMPLEMDE_USE_CDN = True
-
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
 
     @staticmethod
     def init_app(app):
--- instance/config.py
+++ instance/config.py
@@ -1,1 +1,2 @@
 SECRET_KEY = '3a2922bb04be7e262a9649f0cf3a58d7'
+DATABASE_URL = 'postgresql+psycopg2://vikki:sasawa@localhost/pitches'
